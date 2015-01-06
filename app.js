98c7c34b44b340b1274af53f4fcbe686766f1ae5
marthzelena <martha.elena.loera@gmail.com>
2015-01-06 17:31:03 -0600

    bla

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
+});
 
-app.listen(3000)
+var server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
